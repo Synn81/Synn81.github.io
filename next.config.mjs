38b416976846fbfc6a0f58db94933c915b474fb9
Merrickfox <merrick.fox@gmail.com>
2023-06-09 16:56:38 +0100

    template push

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+  assetPrefix: isProd ? './' : '',
+  images: {
+    unoptimized: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
